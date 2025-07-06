f60cd90405796ad28fb992e83ab57a74fd4b8bd1
Argan khan <khanargan21@gmail.com>
2025-07-06 18:13:35 +0530

    Create Calculate_Function

--- Calc_history.py
+++ Calc_history.py
@@ -16,7 +16,38 @@ def clear_history():
     file.close()
     print("History Cleared.")
 
+
 def save_to_history(equation, result):
     file = open(HISTORY_FILE, 'a')
     file.write(equation + "=" + str(result) + "\n")
     file.close()
+
+def Calculate(user_input):
+    parts = user_input.split()
+    if len(parts) != 3:
+        print("Invalid input. Use format: number operator (e.g 8 + 8)")
+        return
+    
+    num1 = float(parts[0])
+    op = parts[1]
+    num2 = float(parts[2])
+
+    if op == "+":
+        result = num1 + num2
+    elif op == "-":    
+         result = num1 - num2
+    elif op == "*":
+         result = num1 * num2
+    elif op == "/":
+          if num2 == "0":
+              print("Cannot divide by zero")
+          result = num1 / num2
+    else:
+        print("Invalid operator. Use only + - * /.")
+        return                
+
+    if float(result) == result:
+        result = int(result)
+    print("Result:", result)   
+
+    save_to_history(user_input, result) 
